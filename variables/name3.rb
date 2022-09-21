d1c203d0c509d738dbc92a2221f4ba2d3669144c
Tim Durand <oatte@pm.me>
2022-09-21 18:02:05 -0500

    variables exercise 4

---
+++ variables/name3.rb
@@ -0,0 +1,7 @@
+puts "You will surrender your first name!"
+first_name = gets.chomp
+
+puts "Now reveal to me your last name!"
+last_name = gets.chomp
+
+puts "Greetings, " + first_name + " " + last_name + "!"
